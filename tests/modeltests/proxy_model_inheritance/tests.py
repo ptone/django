30e6394728d01232434cc242ada51cd537db4724
Preston Holmes <preston@ptone.com>
2012-07-06 10:52:36 -0700

    fixing up tests using installed_apps override

--- tests/modeltests/proxy_model_inheritance/tests.py
+++ tests/modeltests/proxy_model_inheritance/tests.py
@@ -3,9 +3,10 @@ from __future__ import absolute_import
 import os
 import sys
 
+from django.apps import cache
 from django.conf import settings
 from django.core.management import call_command
-from django.db.models.loading import cache, load_app
+from django.db.models.signals import post_syncdb
 from django.test import TestCase, TransactionTestCase
 from django.test.utils import override_settings
 
@@ -13,7 +14,6 @@ from .models import (ConcreteModel, ConcreteModelSubclass,
     ConcreteModelSubclassProxy)
 
 
-@override_settings(INSTALLED_APPS=('app1', 'app2'))
 class ProxyModelInheritanceTests(TransactionTestCase):
     """
     Proxy model inheritance across apps can result in syncdb not creating the table
@@ -24,21 +24,23 @@ class ProxyModelInheritanceTests(TransactionTestCase):
     def setUp(self):
         self.old_sys_path = sys.path[:]
         sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-        map(load_app, settings.INSTALLED_APPS)
+        self.sync_receivers = post_syncdb.receivers
+        post_syncdb.receivers = []
 
     def tearDown(self):
         sys.path = self.old_sys_path
-        del cache.app_store[cache.app_labels['app1']]
-        del cache.app_store[cache.app_labels['app2']]
-        del cache.app_labels['app1']
-        del cache.app_labels['app2']
-        del cache.app_models['app1']
-        del cache.app_models['app2']
+        post_syncdb.receivers = self.sync_receivers
+        cache._reset()
 
+    @override_settings(INSTALLED_APPS=(
+        'modeltests.proxy_model_inheritance.app1',
+        'modeltests.proxy_model_inheritance.app2',
+        ))
     def test_table_exists(self):
+        cache._reload()
         call_command('syncdb', verbosity=0)
-        from .app1.models import ProxyModel
-        from .app2.models import NiceModel
+        from modeltests.proxy_model_inheritance.app1.models import ProxyModel
+        from modeltests.proxy_model_inheritance.app2.models import NiceModel
         self.assertEqual(NiceModel.objects.all().count(), 0)
         self.assertEqual(ProxyModel.objects.all().count(), 0)
 
