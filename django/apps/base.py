b0edef2f1fb48535f89459caa45f0876b2b47819
Preston Holmes <preston@ptone.com>
2012-07-06 13:23:57 -0700

    move parent model traversal into App class

--- django/apps/base.py
+++ django/apps/base.py
@@ -65,3 +65,15 @@ class App(object):
         upper = lambda match: match.group(1).upper()
         cls_name = module_name_re.sub(upper, name.split('.')[-1])
         return type(cls_name[0].upper()+cls_name[1:], (cls,), {'_name': name})
+
+    def add_parent_models(self):
+        from django.apps import cache
+        parents = [p for p in self.__class__.mro()
+                    if hasattr(p, '_meta')]
+        for parent in reversed(parents):
+            parent_models = cache.app_models.get(parent._meta.label, {})
+            # update app_label and installed attribute of parent models
+            for model in parent_models.itervalues():
+                model._meta.app_label = self._meta.label
+                model._meta.installed = True
+            self._meta.models.update(parent_models)
