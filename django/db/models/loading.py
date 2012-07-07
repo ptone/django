e7301e68a32254fec15241c9a97fbf2db36b29e1
Preston Holmes <preston@ptone.com>
2012-07-07 15:00:01 -0700

    locate module level representation only to loading.py

--- django/db/models/loading.py
+++ django/db/models/loading.py
@@ -1,13 +1,30 @@
 "Utilities for loading models and the modules that contain them."
 import warnings
 
+from django.apps import App, AppCache, cache
+
+__all__ = (
+    'App', 'get_apps', 'get_app', 'get_app_errors',
+    'get_models', 'get_model', 'register_models', 'load_app',
+    'app_cache_ready'
+)
+
+
 # Imported here for backwards compatibility
-from django.apps import (App, AppCache, cache,
-    find_app, get_apps, get_app, get_app_errors, get_models, get_model,
-    register_models, load_app, app_cache_ready)
+
+# These methods were always module level, so are kept that way for backwards
+# compatibility.
+get_apps = cache.get_apps
+get_app = cache.get_app
+get_app_errors = cache.get_app_errors
+get_models = cache.get_models
+get_model = cache.get_model
+register_models = cache.register_models
+load_app = cache.load_app
+app_cache_ready = cache.app_cache_ready
 
 warnings.warn(
     'The utilities in django.db.models.loading have been moved to '
-    'django.apps. Please update your code accordingly.',
+    'django.apps.cache. Please update your code accordingly.',
     PendingDeprecationWarning)
 
