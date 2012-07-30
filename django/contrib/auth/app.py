1bbacb00cdfc8c44a702c12fabb1d9196156ace3
Preston Holmes <preston@ptone.com>
2012-07-29 18:04:09 -0700

    ensure that custom user model gets loaded

--- django/contrib/auth/app.py
+++ django/contrib/auth/app.py
@@ -1,4 +1,5 @@
 from django import apps
+from django.utils.importlib import import_module
 from django.utils.translation import ugettext_lazy as _
 
 class AuthApp(apps.App):
@@ -8,5 +9,11 @@ class AuthApp(apps.App):
         verbose_name = _('auth')
 
     def get_user_model(self):
-        model_name = self.auth_user_model.split('.')[-1].lower()
+        model_name = self.user_model.split('.')[-1].lower()
+        if model_name not in self._meta.models:
+            self.register_models()
         return self._meta.models[model_name]
+
+    def register_models(self):
+        super(AuthApp, self).register_models()
+        import_module(self.user_model.rsplit('.', 1)[0])
