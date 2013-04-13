86718d332d1f4844ed1a0d7562be51b6b32a09d1
Preston Holmes <preston@ptone.com>
2013-04-12 22:15:01 -0700

    more backref killing - sites tests all pass

--- django/contrib/sites/tests.py
+++ django/contrib/sites/tests.py
@@ -13,10 +13,10 @@ class SitesFrameworkTests(TestCase):
     def setUp(self):
         Site(id=settings.SITE_ID, domain="example.com", name="example.com").save()
         self.old_Site_meta_installed = Site._meta.installed
-        Site._meta.app.installed = True
+        Site._meta.installed = True
 
     def tearDown(self):
-        Site._meta.app.installed = self.old_Site_meta_installed
+        Site._meta.installed = self.old_Site_meta_installed
 
     def test_save_another(self):
         # Regression for #17415
@@ -67,7 +67,7 @@ class SitesFrameworkTests(TestCase):
         self.assertRaises(ObjectDoesNotExist, get_current_site, request)
 
         # A RequestSite is returned if the sites framework is not installed
-        Site._meta.app.installed = False
+        Site._meta.installed = False
         site = get_current_site(request)
         self.assertTrue(isinstance(site, RequestSite))
         self.assertEqual(site.name, "example.com")
